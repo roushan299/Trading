ba873b9f6795d522b496d8b658b8c39a647cf7b4
Roushan kumar <roushankumar7231246@gmail.com>
2024-09-15 09:05:31 +0530

    trading project created

---
+++ src/main/java/com/roushan/trading/model/User.java
@@ -0,0 +1,21 @@
+package com.roushan.trading.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    private Long id;
+
+
+}
